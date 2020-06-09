c0026336a7cc39a1651ee970d67feccd4627eab9
eduardo.cgouvea <ecgouvea@hotmail.com>
2020-06-08 21:31:22 -0300

    Add socket connection test

---
+++ src/main/java/com/ecgouvea/example/conectividade/controller/SocketConnectController.java
@@ -0,0 +1,38 @@
+package com.ecgouvea.example.conectividade.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.sql.*;
+import java.util.Date;
+
+@RestController
+public class SocketConnectController {
+
+    @GetMapping("/teste/socket")
+    public String testeSocket(
+            @RequestParam(required = false) String host,
+            @RequestParam(required = false) Integer port
+    ) throws Exception {
+        String values = "";
+
+        try (
+                Socket clientSocket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+           values = in.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+            values = e.getMessage();
+        }
+
+        return new Date().toString() + "<br>Result:<br>" + values;
+    }
+
+}
